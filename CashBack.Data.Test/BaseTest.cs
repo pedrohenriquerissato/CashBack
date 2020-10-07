3fa3729e9b52ed2437a62d69fb23ea913e392e4e
Pedro Rissato <pedro.rissato@bitzar.solutions>
2020-10-07 15:46:49 -0300

    Fixed test pattern class

--- CashBack.Data.Test/BaseTest.cs
+++ CashBack.Data.Test/BaseTest.cs
@@ -6,41 +6,41 @@ using Xunit;
 
 namespace CashBack.Data.Test
 {
-    public abstract class BaseTest
+  public abstract class BaseTest
+  {
+    public BaseTest()
     {
-        public BaseTest()
-        {
 
-        }
+    }
+  }
+
+  public class DbTest : IDisposable
+  {
+    private string databaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", "")}";
+
+    public ServiceProvider ServiceProvider { get; private set; }
+
+    public DbTest()
+    {
+      var serviceCollection = new ServiceCollection();
+      serviceCollection.AddDbContext<CashBackContext>(
+          options => options.UseSqlServer($"server=localhost; database={databaseName}; user id=sa; password=yourStrong(!)Password"),
+          ServiceLifetime.Transient
+          );
+
+      ServiceProvider = serviceCollection.BuildServiceProvider();
+      using (var context = ServiceProvider.GetService<CashBackContext>())
+      {
+        context.Database.EnsureCreated();
+      }
     }
 
-    public class DbTest : IDisposable
+    public void Dispose()
     {
-        private string databaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", "")}";
-
-        public ServiceProvider ServiceProvider { get; private set; }
-
-        public DbTest()
-        {
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddDbContext<CashBackContext>(
-                options => options.UseSqlServer($"server=localhost; database={databaseName}; user id=sa; password=yourStrong(!)Password"),
-                ServiceLifetime.Transient
-                );
-
-            ServiceProvider = serviceCollection.BuildServiceProvider();
-            using (var context = ServiceProvider.GetService<CashBackContext>())
-            {
-                context.Database.EnsureCreated();
-            }
-        }
-
-        public void Dispose()
-        {
-            using (var context = ServiceProvider.GetService<CashBackContext>())
-            {
-                context.Database.EnsureDeleted();
-            }
-        }
+      using (var context = ServiceProvider.GetService<CashBackContext>())
+      {
+        context.Database.EnsureDeleted();
+      }
     }
+  }
 }
---
+++ CashBack.Data.Test/RetailerRepositoryTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CashBack.Data.Context;
+using CashBack.Data.Repositories;
+using CashBack.Domain.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CashBack.Data.Test
+{
+  public class RetailerRepositoryTest : BaseTest, IClassFixture<DbTest>
+  {
+    private ServiceProvider serviceProvider;
+
+    public RetailerRepositoryTest(DbTest dbTest)
+    {
+      serviceProvider = dbTest.ServiceProvider;
+    }
+
+    [Fact(DisplayName = "Retailer CRUD")]
+    [Trait("CRUD", nameof(Retailer))]
+    public async Task Is_Possible_Retailer_Crud()
+    {
+      using (var context = serviceProvider.GetService<CashBackContext>())
+      {
+        var repository = new RetailerRepository(context);
+        var retailer = new Retailer
+        {
+          // Fake - Generated at: https://www.4devs.com.br/gerador_de_cpf
+          DocumentId = "715.244.760-80",
+          Email = Faker.Internet.Email(),
+          FullName = Faker.Name.FullName(),
+          Password = Faker.Lorem.GetFirstWord(),
+        };
+
+        var resultInsert = await repository.CreateRetailer(retailer);
+        Assert.NotNull(resultInsert);
+        Assert.Equal(retailer.Email, resultInsert.Email);
+        Assert.True(retailer.Id == resultInsert.Id);
+        Assert.True(resultInsert.Id != 0);
+
+        retailer.FullName = Faker.Name.FullName();
+        var resultUpdate = await repository.UpdateRetailer(retailer);
+        Assert.NotNull(resultUpdate);
+        Assert.Equal(retailer.Email, resultUpdate.Email);
+        Assert.Equal(retailer.FullName, resultUpdate.FullName);
+
+        var resultRead = await repository.GetWithPurchaseByDocumentIdAsync(resultInsert.DocumentId);
+        Assert.NotNull(resultRead);
+        Assert.True(retailer.Id == resultRead.Id);
+        Assert.True(resultInsert.FullName == resultRead.FullName);
+
+        var resultDelete = await repository.DeleteRetailer(resultInsert);
+        Assert.True(resultDelete);
+      }
+    }
+  }
+}
